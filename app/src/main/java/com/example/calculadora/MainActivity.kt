2afc42080a3f9712e310ad0471d5768fe6f66b37
Roger28 <rogersouza635@gmail.com>
2019-05-29 02:07:37 -0300

    Novo layout e refatoração de código.
           * Criado novo layout com TableLayout e TableRows.
           * Removido código extenso que verificava qual cardView era
           clicado e inserido métodos de acordo com os tipos de botões
           existente no layout (digito, operador, ponto decimal...).

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,105 +1,80 @@
 package com.example.calculadora
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
-    private var primeiroValor: Float = 0F
-    private var segundoValor: Float = 0F
-    private lateinit var operador: String
-    private lateinit var numeros: ArrayList<String>
+    private var lastDigitWasNumeric: Boolean = false
+    private var isInvalideOperation: Boolean = false
+    private var lastDigitWasDecimalPoint: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        numeros = ArrayList()
     }
 
-    @SuppressLint("SetTextI18n")
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            cardView_um.id -> {
-                numeros.add(textView_um.text.toString())
-                textView_primeiro_valor.append(textView_um.text)
-            }
-            cardView_dois.id -> {
-                numeros.add(textView_dois.text.toString())
-                textView_primeiro_valor.append(textView_dois.text)
-            }
-            cardView_tres.id -> {
-                numeros.add(textView_tres.text.toString())
-                textView_primeiro_valor.append(textView_tres.text)
-            }
-            cardView_quatro.id -> {
-                numeros.add(textView_quatro.text.toString())
-                textView_primeiro_valor.append(textView_quatro.text)
-            }
-            cardView_cinco.id -> {
-                numeros.add(textView_cinco.text.toString())
-                textView_primeiro_valor.append(textView_cinco.text)
-            }
-            cardView_seis.id -> {
-                numeros.add(textView_seis.text.toString())
-                textView_primeiro_valor.append(textView_seis.text)
-            }
-            cardView_sete.id -> {
-                numeros.add(textView_sete.text.toString())
-                textView_primeiro_valor.append(textView_sete.text)
-            }
-            cardView_oito.id -> {
-                numeros.add(textView_oito.text.toString())
-                textView_primeiro_valor.append(textView_oito.text)
-            }
-            cardView_nove.id -> {
-                numeros.add(textView_nove.text.toString())
-                textView_primeiro_valor.append(textView_nove.text)
-            }
-            cardView_ponto.id -> {
-                numeros.add(textView_um.text.toString())
-                textView_primeiro_valor.append(textView_ponto.text)
-            }
-            cardView_igualdade.id -> {
-                calculaResultado(textView_resultado)
-            }
-            cardView_adicao.id -> {
-                numeros.add(textView_adicao.text.toString())
-                textView_operador.text = textView_adicao.text
-                textView_resultado.id = textView_adicao.id
-            }
-            cardView_subtracao.id -> {
-                numeros.add(textView_subtracao.text.toString())
-                textView_operador.text = textView_subtracao.text
-            }
-            cardView_multiplicacao.id -> {
-                numeros.add(textView_multiplicacao.text.toString())
-                textView_operador.text = textView_multiplicacao.text
-            }
-            cardView_divisao.id -> {
-                numeros.add(textView_divisao.text.toString())
-                textView_operador.text = textView_divisao.text
-            }
-            cardView_limpar.id -> {
-                textView_primeiro_valor.text = ""
-                textView_operador.text = ""
-                textView_segundo_valor.text = ""
-                textView_resultado.text = ""
+    fun onDigit(v: View) {
+        if (this.isInvalideOperation) {
+            textView_entrada_usuario.text = (v as Button).text
+            this.isInvalideOperation = false
+        } else {
+            textView_entrada_usuario.append((v as Button).text)
+        }
+        this.lastDigitWasNumeric = true
+    }
 
-            }
+    fun onOperator(v: View) {
+        if (this.lastDigitWasNumeric && !this.isInvalideOperation) {
+            textView_entrada_usuario.append((v as Button).text)
+            this.lastDigitWasNumeric = false
+            this.lastDigitWasDecimalPoint = false
+        } else {
+            dialog()
         }
     }
 
-    private fun calculaResultado(textView_operador: View?) {
-        when (textView_operador?.id) {
-            textView_adicao.id -> {
-                primeiroValor = textView_primeiro_valor.toString().toFloat()
-                segundoValor = textView_segundo_valor.toString().toFloat()
-                textView_resultado.text = primeiroValor.plus(segundoValor).toString()
-            }
+    fun onDecimalPoint(v: View) {
+        if (this.lastDigitWasNumeric && !this.isInvalideOperation && !this.lastDigitWasDecimalPoint) {
+            textView_entrada_usuario.append((v as Button).text)
+            this.lastDigitWasNumeric = false
+            this.lastDigitWasDecimalPoint = false
         }
+    }
+
+    fun onEqual(v: View) {
+        if (this.lastDigitWasNumeric && !isInvalideOperation) {
+            try {
+                val resultado = ExpressionBuilder(textView_entrada_usuario.text.toString()).build().evaluate()
+                textView_entrada_usuario.text = resultado.toString()
+                lastDigitWasDecimalPoint = true
+            }catch (e: ArithmeticException){
+                this.isInvalideOperation = true
+                this.lastDigitWasNumeric = false
+            }
+        } else {
+            dialog()
+        }
+    }
+
+    fun onClear(v: View) {
+        textView_entrada_usuario.text = ""
+        this.lastDigitWasNumeric = false
+        this.isInvalideOperation = false
+        this.lastDigitWasDecimalPoint = false
+    }
+
+    private fun dialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.operacao_invalida))
+            .setMessage(getString(R.string.digite_operacao_valida))
+            .setPositiveButton("OK") { _, _ -> }
+            .show()
     }
 
 }
